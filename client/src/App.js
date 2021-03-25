c8a68f5e42b1c3506a68163e4786916b8f7d275e
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-03-26 02:56:50 +0530

    front-end for client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+// here we import all the components and provide routing.we install router dom so we cam have one route to login page,logout page and so on
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  );
+};
+
+export default App;
